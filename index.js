ca0a77d5e931a715843756776da455c9ff3caad4
Adrian Preoteasa <adrian.preoteasa@siveco.ro>
2017-07-10 19:11:37 +0300

    Modify recursivity to include holder, include more conditions to chosen resource paths

--- index.js
+++ index.js
@@ -28,18 +28,28 @@ var _ = require('underscore');
  */
 function getLinksFromJSONs(callback) {
 	var resultsArray = [];
+	var totalFiles;
+	var fileIndex = 0;
 	glob(opts.pathToResources + "/**/*.json", function(err, files) {
+		totalFiles = files.length;
 		files.forEach(function(fileToParse) {
-			console.log('Reading ', fileToParse);
 			fs.readJSON(fileToParse, function(err, obj) {
-				obj && _.each(obj.rlos, function(activity) {
-					if(activity.resources) {
-						_.each(activity.resources, function(resource) {
-							recursivePass(resource, resultsArray);
+				fileIndex++;
+				//console.log('Working on file number ', fileIndex, ' path ', fileToParse);
+				if(!obj) {
+					console.log('!!!!!!!!!! Error reading file ', fileToParse);
+				} else {
+					_.each(obj.rlos, function(activity) {
+						var resultPerActivity = [];
+						recursivePassHolder(activity, resultPerActivity, function() {
+							resultsArray.push(resultPerActivity);
 						});
-						callback(resultsArray);
-					}
-				});			
+					});
+				}
+
+				if(fileIndex == totalFiles) {
+					callback(resultsArray);
+				}	
 			});
 		});		
 	})	
@@ -51,10 +61,29 @@ function getLinksFromJSONs(callback) {
  * @param  {Array} resultsArray  Array in which the desired links are added
  */
 function recursivePass(targetObject, resultsArray) {
+	//console.log('recursive')
 	if(typeof targetObject !== 'object') return;
 	for(var i in targetObject) {
-		i === "link" && targetObject[i].indexOf('img') < 0 && targetObject[i].indexOf('http') < 0 ? resultsArray.push(targetObject[i]) : recursivePass(targetObject[i], resultsArray);
+		i === "link" 
+			&& targetObject[i].length > 0
+			&& targetObject[i].indexOf('img/') < 0 
+			&& targetObject[i].indexOf('http:') < 0 
+			&& targetObject[i].indexOf('youtube.') < 0 
+			&& targetObject[i].indexOf('youtu.') < 0 
+			&& targetObject[i].indexOf('vimeo.') < 0 
+			&& targetObject[i].indexOf('ted.') < 0 
+			? resultsArray.push(targetObject[i]) : recursivePass(targetObject[i], resultsArray);
 	}
+}
+
+/**
+ * Holder for the recursive function to make sure we get the final parsed result
+ * @param  {Object}   targetObject Target object that must be parsed
+ * @param  {Array}    resultsArray Array thay will hold the result
+ * @param  {Function} callback     Callback function to be cakked on the result of the recursive function
+ */
+function recursivePassHolder(targetObject, resultsArray, callback) {
+	callback(recursivePass(targetObject, resultsArray));
 }
 
 /**
@@ -62,6 +91,8 @@ function recursivePass(targetObject, resultsArray) {
  * @param  {Array} data Array of found links
  */
 function writeToResultFile(data) {
+	console.log('########## Writing to result file ##########');
+	data = _.flatten(data);
 	var preparedData = data.map(function(element){
 		return element.split('/').slice(-3).join(',');
 	})
